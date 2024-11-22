ab3f027ed6353731955abd57b6e324f1d4f2e4bf
DezieleInm <deziele.silva@inmetrics.com.br>
2024-11-22 09:19:59 -0300

    Update shopping.cy.js

--- cypress/e2e/shopping.cy.js
+++ cypress/e2e/shopping.cy.js
@@ -3,18 +3,16 @@ describe('Cenário 1: Buscar um produto', () => {
     // Passo 1: Acessar o site
     cy.visit('https://advantageonlineshopping.com/#/');
     
-    
     // Passo 2: Clicar no botão de pesquisa
     cy.get('#mobileSearch > #menuSearch').click(); 
     
-     
-   // Adionar um produto no carrinho
+    // Passo 3: Adionar um produto no carrinho
     cy.get('[data-ng-show="([] | productsFilterForCategoriesProduct:searchResult:minPriceToFilter:maxPriceToFilter:productsInclude).length != 0"] > ul > :nth-child(1)').first().click();
 
-   // Clicar  no carrinho
+   // Passo 4: Clicar  no carrinho
    cy.get('.fixedBtn > .roboto-medium').click();
 
-   // validar produto no carrinho
+   // Passo 5: validar produto no carrinho
    cy.get('li > #toolTipCart > :nth-child(1) > table > tfoot > :nth-child(2) > td > #checkOutPopUp').click();
 
   });
